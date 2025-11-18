f967a56b50a66cfd74d81757ce87d97fd8b25eac
antonio <antonio.saenz@rai.usc.es>
2025-11-18 19:14:00 +0100

    Actualizacion valor bote de float a int

--- monopoly/casillas/Especial.java
+++ monopoly/casillas/Especial.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 /**
  * Casilla especial: Salida, Carcel, Parking, IrCarcel, ...
- * Necesita referencia al Tablero para localizar otras casillas (p. ej. la cárcel).
  */
 public class Especial extends Casilla {
     private Tablero tablero;
@@ -22,37 +21,39 @@ public class Especial extends Casilla {
 
     @Override
     public boolean evaluarCasilla(Jugador actual, Jugador banca, int tirada) {
-        // Usamos la posición para decidir la acción y así no dependemos del texto exacto del nombre.
+
         int pos = getPosicion();
 
         switch (pos) {
+
             case 0: // Salida
-                // Normalmente pasar por salida se gestiona en moverYEvaluar (suma de vuelta),
-                // aquí solo informamos al jugador de que ha caído/pasado por Salida.
                 System.out.println(actual.getNombre() + " ha pasado por la salida.");
                 return true;
 
-            case 10: // Carcel (casilla de visita / carcel)
-                // Caer en la casilla de la cárcel no suele enviar a la cárcel, es visita.
+            case 10: // Cárcel (visita)
                 System.out.println(actual.getNombre() + " está en la casilla de la cárcel (visita).");
                 return true;
 
-            case 20: // Parking  (bote)
-                // Si quieres usar la casilla Parking como depósito, usa getValor()/setValor.
-                float bote = Impuestos.getBote();
+            case 20: // Parking con bote (entero)
+                int bote = Impuestos.getBote();
+
                 if (bote > 0) {
                     actual.recibir(bote);
-                    actual.acumularPremiosInversionesOBote(bote); //Para almacenar os datos en estadisticas
-                    Impuestos.setBote(0);//Vaciamos o bote
-                    System.out.printf("%s ha recibido %.0f del Parking gratuito.%n", actual.getNombre(), bote);
+                    actual.acumularPremiosInversionesOBote(bote);
+                    Impuestos.setBote(0);
+
+                    System.out.printf("%s ha recibido %d del Parking gratuito.%n",
+                            actual.getNombre(), bote);
+
                 } else {
                     System.out.println(actual.getNombre() + " ha caído en Parking (sin bote).");
                 }
+
                 return true;
 
             case 30: // Ir a la cárcel
-                // Enviar a la cárcel: buscamos la casilla de la cárcel (pos 10) y la pasamos al jugador.
                 Casilla carcel = tablero.getCasilla(10);
+
                 if (carcel != null) {
                     actual.enviarACarcel(carcel);
                     System.out.println(actual.getNombre() + " ha sido enviado a la cárcel.");
@@ -62,7 +63,6 @@ public class Especial extends Casilla {
                 return true;
 
             default:
-                // Comportamiento por defecto para otras casillas especiales
                 System.out.println(actual.getNombre() + " ha caído en una casilla especial: " + getNombre());
                 return true;
         }
@@ -72,35 +72,39 @@ public class Especial extends Casilla {
     public void comprarCasilla(Jugador solicitante, Jugador banca) {
         System.out.println("No puedes comprar una casilla de especial.");
     }
+
     @Override
     public String infoCasilla() {
         StringBuilder sb = new StringBuilder();
         sb.append("{ ");
 
         switch (getPosicion()) {
-            case 10: // Carcel
+
+            case 10: // Cárcel
                 sb.append("salir: ").append(Valor.SALIR_CARCEL).append(", ");
                 break;
-            case 20: // Parking
-                Impuestos impuesto=new Impuestos();
-                sb.append("bote: ").append(impuesto.getBote()).append(", ");
-                break;
-            default:
 
+            case 20: // Parking
+                sb.append("bote: ").append(Impuestos.getBote()).append(", ");
+                break;
+
+            default:
                 break;
         }
 
         sb.append("jugadores: [");
+
         List<Avatar> avs = getAvatares();
         for (int i = 0; i < avs.size(); i++) {
             Avatar a = avs.get(i);
             sb.append(a.getId());
-            // si es cárcel, añadimos turnos
+
             if (getPosicion() == 10) {
                 sb.append(",").append(a.getJugador().getTurnosEnCarcel());
             }
             if (i < avs.size() - 1) sb.append("] [");
         }
+
         sb.append("] }");
 
         return sb.toString();
@@ -108,6 +112,6 @@ public class Especial extends Casilla {
 
     @Override
     public String casEnVenta() {
-        return ""; //No se puede vender
+        return "";
     }
 }
--- monopoly/casillas/Impuestos.java
+++ monopoly/casillas/Impuestos.java
@@ -8,11 +8,12 @@ import partida.Jugador;
  */
 public class Impuestos extends Casilla {
 
-    private float impuesto;
-    private static float bote=0;//Static para que todas as instancias o compartan
+    private int impuesto;
+    private static int bote = 0; // Static para que todas las instancias lo compartan
+
     // Constructor
-    public Impuestos(String nombre, int posicion, float impuesto, Jugador duenho) {
-        super(nombre, posicion, impuesto, duenho); // usa el constructor específico de impuestos
+    public Impuestos(String nombre, int posicion, int impuesto, Jugador duenho) {
+        super(nombre, posicion, impuesto, duenho); 
         this.impuesto = impuesto;
     }
 
@@ -24,18 +25,29 @@ public class Impuestos extends Casilla {
     public boolean evaluarCasilla(Jugador actual, Jugador banca, int tirada) {
         if (actual.getFortuna() >= impuesto) {
             actual.pagar(impuesto);
+
+            // Sumar al bote (entero)
             setBote(impuesto);
-            System.out.println("El jugador paga "+ impuesto +"€ que se depositan en el Parking.");
+
+            System.out.println("El jugador paga " + impuesto + "€ que se depositan en el Parking.");
             banca.recibir(impuesto);
             return true;
-        } else {
 
+        } else {
             return false;
         }
     }
-    public static float setBote(float impuesto){
-        return bote+=impuesto;
+
+    // Ahora el bote es int, así que el setter también
+    public static int setBote(int cantidad) {
+        bote += cantidad;
+        return bote;
     }
+
+    public static int getBote() {
+        return bote;
+    }
+
     @Override
     public void comprarCasilla(Jugador solicitante, Jugador banca) {
         System.out.println("No puedes comprar una casilla de impuestos.");
@@ -43,22 +55,14 @@ public class Impuestos extends Casilla {
 
     @Override
     public String infoCasilla() {
-
         return String.format("{%n" +
                 "tipo: impuesto,%n" +
-                "apagar: %.0f%n" +
+                "apagar: %d%n" +
                 "}", impuesto);
     }
 
     @Override
     public String casEnVenta() {
-        return ""; // No  está en venta
+        return ""; 
     }
-
-    public static float getBote() {
-        return bote;
-}
-
-
-
 }
