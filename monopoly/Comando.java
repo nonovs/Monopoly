879bedf903184084c9a54c6de3739e042860046e
nonovs <elpaspon@gmail.com>
2025-11-26 10:43:04 +0100

    Comentario

--- monopoly/Comando.java
+++ monopoly/Comando.java
@@ -1,6 +1,9 @@
 package monopoly;
 
 public interface Comando {
+    /// default void metododefecto(){
+    /// System.out.printnl("HOlacaracola)
+    /// } esto simplemente era para probar un metodo por defecto
     void crearJugador(String nombre, String tipoAvatar);
     void mostrarJugadorEnTurno();
     void listarJugadores();
